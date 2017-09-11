9dbfe155c380901f38feed8284a23e7fca91692b
haihm <haihmse04631@fpt.edu.vn>
2017-09-11 20:43:38 +0700

    update code

---
+++ Source/First_Greeting/app/src/main/java/com/example/haihm/firstgreeting/RoomVideoCall.java
@@ -0,0 +1,13 @@
+package com.example.haihm.firstgreeting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoomVideoCall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_video_call);
+    }
+}
--- Source/First_Greeting/app/src/main/java/com/example/haihm/firstgreeting/VideoCall.java
+++ Source/First_Greeting/app/src/main/java/com/example/haihm/firstgreeting/VideoCall.java
@@ -1,20 +1,34 @@
 package com.example.haihm.firstgreeting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by haihm on 8/8/2017.
  */
 
 public class VideoCall extends Fragment {
+    Button btnAttendance;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.video_call_tab, container, false);
+
+        btnAttendance = (Button) rootView.findViewById(R.id.btnAttendance);
+
+        btnAttendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), RoomVideoCall.class);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
     }
 }
