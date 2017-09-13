245e475321537da5c340bef25c997d0101c6499a
SonPH <hongsongp97@gmail.com>
2017-09-13 18:50:59 +0700

    Add user video call

--- sever/app.js
+++ sever/app.js
@@ -10,87 +10,136 @@ const API_SECRET = 'd79c3cfa1c01f93d4b1c461c8494ff1b9b5e75fe';
 
 // get initial session id 
 var opentok = new OpenTok(API_KEY, API_SECRET);
-var m = 0, n = 0;
-var a = new Array();
-var b = new Array();
+var sessionId = [];
+var m = 0,
+    n = 0;
+var member = [];
+var cvt = [];
+var memberWait = [];
+var cvtWait = [];
 
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i].data.id === obj.data.id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Array.prototype.push = function(obj) {
+    this[this.length++] = obj;
+}
+
+Array.prototype.get = function(i) {
+    return this[i];
+}
 
 function scramble(array) {
     var tmp, current, top = array.length;
 
-    if(top) while(--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-    }
+    if (top)
+        while (--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = array[current];
+            array[current] = array[top];
+            array[top] = tmp;
+        }
     return array;
 }
 
-a = scramble(a); b = scramble(b);
-
-
-io.on('connection', function (socket) {
-
-    console.log('A user connected ');
-
-    //---- load client data list ----
-
-    socket.on('client-send', function(data){
-        console.log(data);
-        //==== storage data from client
-        if (data.role === "member"){
-            b[++n] = {id : data.id, socket : socket} 
-        }else{
-            a[++m] = {id : data.id, socket : socket}
-        }
-        io.emit('server-send', {noidung: 'ok'});            
-    })
-
-    // socket.on('get-session-id', function(data) {
-    //     // return session id for client
-    //     io.emit('return-session-id', {sessionId : sessionId, token : opentok.generateToken(sessionId)});
-    // });
-}); 
-
-
-
-//------- set time to start every calls?? ------ 
-
-
-//------- send sessionId and token for each client --------
-var sessionId;
-
-var n = Math.min(n, Math.trunc(m/2))
-
-for (var i = 1; i <= n; i++){
-    opentok.createSession(function(error, session) {
-
-        // console.log(API_KEY);
-        // console.log(API_SECRET);
-
-        if (error) {
-            console.log("Error creating session:", error)
-        } else {
-            sessionId = session.sessionId;
-
-            //---- sending ---
-            a[i].socket.emit('return-session-id', {sessionId : sessionId, token: opentok.generateToken(sessionId)});
-            b[i*2-1].socket.emit('return-session-id', {sessionId : sessionId, token: opentok.generateToken(sessionId)});
-            b[i*2].socket.emit('return-session-id', {sessionId : sessionId, token: opentok.generateToken(sessionId)});
-
-
-            console.log("Session ID: " + sessionId);
-        }
-
-    });
-        
-}
-    
-
+member = scramble(member);
+cvt = scramble(cvt);
 
 server.listen(3000, () => {
     console.log(`Server is running at localhost:3000`);
 });
 
+io.on('connection', function(socket) {
 
+    console.log('A user connected ');
+
+    socket.on('get-session-id', function(data) {
+         {
+            //==== storage data from client
+            if (data.role === "Member") {
+                if (!member.contains(data)) {
+                    member.push({ id: data.id, socket: socket });
+                    memberWait[member.length-1] = true;
+                    console.log(member.length);
+                    var check = setInterval(function() {
+                    if (!memberWait[member.length-1]) {
+                        console.log("Session ID: " + sessionId);
+                        socket.emit('return-session-id', { sessionId: sessionId, token: opentok.generateToken(sessionId) });
+                    }
+                }, 1000);
+                }
+            } else if (!cvt.contains(data)){
+                cvt.push({ id: data.id, socket: socket });
+                cvtWait[cvt.length-1] = true;
+                console.log(cvt.length);
+                var check = setInterval(function() {
+                    if (!cvtWait[cvt.length-1]) {
+                        console.log("Session ID: " + sessionId);
+                        socket.emit('return-session-id', { sessionId: sessionId, token: opentok.generateToken(sessionId) });
+                    }
+                }, 1000);
+            }
+            
+            if (cvt.length == 2) start();
+        }
+    });
+});
+
+//------- set time to start every calls?? ------ 
+
+
+//------- send sessionId and token for each client --------
+start();
+// Start
+function start() {
+    console.log("Starting");
+    n = member.length;
+    m = cvt.length;
+    // console.log(n);
+    // console.log(m);
+    // var numberOfSession = Math.max(n, Math.trunc(m / 2))
+    // console.log("max" + numberOfSession);
+    for (var i = 0; i < 10; i++) {
+        console.log(i);
+        opentok.createSession(function(error, session) {
+            console.log(API_KEY);
+            console.log(API_SECRET);
+            
+
+            if (error) {
+                console.log("Error creating session:", error)
+            } else {
+
+                // return session id for client
+
+                sessionId[sessionId.length] = session.sessionId;
+
+                // -- --sending-- -
+
+                console.log("created: " + sessionId.length);
+                // if (i < n) {
+                //     memberWait[i] = false;
+                // }
+                // if (i * 2 < m) {
+                //     cvtWait[i * 2] = false;
+                //     console.log(i*2 + " = " + cvtWait[i*2]);
+                // }
+                // if (i * 2 + 1 < m) {
+                //     cvtWait[i * 2 + 1] = false;
+                //     console.log((i*2+1) + " = ");
+                // }
+
+                console.log("Session ID: " + sessionId);
+            }
+
+        });
+    }
+}
